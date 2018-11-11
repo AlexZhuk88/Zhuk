511d4b15c892e715b291780ff315c57804b62eed
Alex Zhuk <Vizuwi@mail.ru>
2018-11-11 16:37:56 +0200

    Second Check Point

--- database/src/main/java/model/Comment.java
+++ database/src/main/java/model/Comment.java
@@ -4,7 +4,17 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 
 @Data
